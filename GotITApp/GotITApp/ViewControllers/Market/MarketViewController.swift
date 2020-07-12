ac99a377bb5ede33f33bf5778f9ad2eb399afb96
Ruslan <thepw@bk.ru>
2020-07-12 13:54:19 +0300

    Meanless edits

--- GotITApp/GotITApp/ViewControllers/Market/MarketViewController.swift
+++ GotITApp/GotITApp/ViewControllers/Market/MarketViewController.swift
@@ -13,7 +13,7 @@ class MarketViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        navigationItem.title = "huinya"
     }
     
 
