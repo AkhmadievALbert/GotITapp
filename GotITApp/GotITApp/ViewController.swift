9e9bd02738fe01d704d5e11a3112f824ef72ed7e
AkhmadievALbert <65800254+AkhmadievALbert@users.noreply.github.com>
2020-07-12 13:36:15 +0300

    EmptyProlect

---
+++ GotITApp/GotITApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GotITApp
+//
+//  Created by Albert Ahmadiev on 11.07.2020.
+//  Copyright © 2020 Albert Ahmadiev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
